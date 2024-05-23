eb467fe2cceb6eb2242349284e231347a07bf680
Wang Tiaoke <100271394+wngtk@users.noreply.github.com>
2024-05-23 20:44:22 +0800

    Extract component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,6 @@
 import AnecdoteForm from "./components/AnecdoteForm"
 import Anecdotes from "./Anecdotes"
-import {useDispatch} from "react-redux";
-import {filterChange} from "./reducers/filterReducer.js";
-
-function Filter() {
-  const dispatch = useDispatch();
-
-  const handleChange = (event) => {
-    dispatch(filterChange(event.target.value))
-  }
-
-  const style = {
-    margin: 10
-  }
-
-  return (
-    <div>
-      filter <input onChange={handleChange} />
-    </div>
-  )
-}
+import Filter from "./components/Filter"
 
 const App = () => {
   return (
